e83cf4bab395bdf5532dcc4ce23f503e98f56b4c
Syndicalix <sebastien.speich@3wa.io>
2023-02-16 17:15:50 +0100

    first yep theme

---
+++ wp-content/themes/yep/front-page.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <?php wp_head(); ?>  
+        <meta content="<?php echo get_bloginfo( 'name' );?>" name="title">  
+        <meta content="<?php echo get_bloginfo( 'description' );?>" name="description">  
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+        <link rel="preconnect" href="https://fonts.googleapis.com">  
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />  
+<title><?php echo get_bloginfo( 'name' );?></title>
+    </head>
+    <body>
+        <h1>YepFolio<h1>
+        <header>
+            <img></img>
+            <nav>
+                <ul>
+                    <li>
+                    <li>
+                    <li>
+                    <li>
+                </lu>
+            </nav>
+            <section>
+                <h2>YEPFOLIO<h2>
+                    <h3>DÉVELOPPEMENT WEB & MOBILE<h3>
+            </section>
+
+            
+        </header>
+
+        <main>
+            <section>
+                <h2>YEPFOLIO EN QUELQUES MOTS<h2>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+            </section>
+
+            <section>
+                <h2>MES DERNIERS PROJETS<h2>
+                    <article>
+                    </article>
+
+                    <article>
+                    </article>
+
+                    <article>
+                    </article>
+
+                    <article>
+                    </article>
+
+                    <article>
+                    </article>
+            </section>
+        </main>
+
+        <footer>
+            <h2>ME CONTACTER</h2>
+
+            <form>
+                <label>Prénom, Nom*</label>
+                <input></input>
+
+                <label>Email*</label>
+                <input></input>
+
+                <label>Message</label>
+                <textarea></textarea>
+            </form>
+
+        </footer>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
