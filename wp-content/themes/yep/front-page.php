c825f13bb777cd945d7612587d7527afec530426
Syndicalix <sebastien.speich@3wa.io>
2023-02-20 09:56:59 +0100

    aie aie aie

--- wp-content/themes/yep/front-page.php
+++ wp-content/themes/yep/front-page.php
@@ -14,17 +14,62 @@
     </head>
     <body>
         <h1>YepFolio<h1>
-        <header>
-            <img></img>
+        <header id="header">
+            <span>
+            <?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 27.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Calque_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 2250 1500" style="enable-background:new 0 0 2250 1500;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:none;stroke:#FFFFFF;stroke-width:8;stroke-miterlimit:10;}
+	.st1{fill:none;stroke:#EFEC4F;stroke-width:10;stroke-miterlimit:10;}
+	.st2{fill:#EFEC4F;}
+	.st3{fill:none;stroke:#4CEE7E;stroke-width:10;stroke-miterlimit:10;}
+	.st4{fill:#4CEE7E;}
+	.st5{fill:none;stroke:#427CF5;stroke-width:10;stroke-miterlimit:10;}
+	.st6{fill:#427CF5;}
+	.st7{fill:none;stroke:#956CAF;stroke-width:10;stroke-miterlimit:10;}
+	.st8{fill:#956CAF;}
+	.st9{fill:none;stroke:#ECA45C;stroke-width:10;stroke-miterlimit:10;}
+	.st10{fill:#ECA45C;}
+	.st11{fill:none;stroke:#E85B69;stroke-width:10;stroke-miterlimit:10;}
+	.st12{fill:#E85B69;}
+</style>
+<line class="st0" x1="432.6" y1="181.1" x2="130.8" y2="679"/>
+<line class="st0" x1="609.6" y1="1116.9" x2="130.8" y2="679"/>
+<line class="st0" x1="1334.3" y1="984.1" x2="609.6" y2="1116.9"/>
+<line class="st0" x1="2097.2" y1="946.8" x2="1334.3" y2="984.1"/>
+<line class="st0" x1="1334.3" y1="278.9" x2="2097.2" y2="946.8"/>
+<line class="st0" x1="432.6" y1="181.1" x2="1334.3" y2="278.9"/>
+<line class="st0" x1="130.8" y1="679" x2="1334.3" y2="984.1"/>
+<line class="st0" x1="609.6" y1="1116.9" x2="432.6" y2="181.1"/>
+<line class="st0" x1="130.8" y1="679" x2="1343.1" y2="286.7"/>
+<line class="st0" x1="2097.2" y1="946.8" x2="435" y2="194"/>
+<line class="st0" x1="1334.3" y1="984.1" x2="1343.1" y2="286.7"/>
+<line class="st0" x1="609.6" y1="1116.9" x2="1343.1" y2="286.7"/>
+<circle class="st1" cx="432.6" cy="181.1" r="105.6"/>
+<circle class="st2" cx="432.6" cy="181.1" r="71.5"/>
+<circle class="st3" cx="130.8" cy="679" r="105.6"/>
+<circle class="st4" cx="130.8" cy="679" r="71.5"/>
+<circle class="st5" cx="609.6" cy="1116.9" r="105.6"/>
+<circle class="st6" cx="609.6" cy="1116.9" r="71.5"/>
+<circle class="st7" cx="1334.3" cy="984.1" r="105.6"/>
+<circle class="st8" cx="1334.3" cy="984.1" r="71.5"/>
+<circle class="st9" cx="1334.3" cy="278.9" r="105.6"/>
+<circle class="st10" cx="1334.3" cy="278.9" r="71.5"/>
+<circle class="st11" cx="2097.2" cy="946.8" r="105.6"/>
+<circle class="st12" cx="2097.2" cy="946.8" r="71.5"/>
+</svg>
+            </span>
             <nav>
-                <ul>
-                    <li>
-                    <li>
-                    <li>
-                    <li>
-                </lu>
+                <ul id="nav">
+                    <li><a href="#">ACCUEIL</a></li>
+                    <li><a href="#">À PROPOS</a></li>
+                    <li><a href="#">MES PROJETS</a></li>
+                    <li><a href="#">ME CONTACTER</a></li>
+                </ul>
             </nav>
-            <section>
+            <section id="head">
                 <h2>YEPFOLIO<h2>
                     <h3>DÉVELOPPEMENT WEB & MOBILE<h3>
             </section>
@@ -33,34 +78,40 @@
         </header>
 
         <main>
-            <section>
+            <section id="word">
                 <h2>YEPFOLIO EN QUELQUES MOTS<h2>
                     <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
             </section>
 
-            <section>
+            <section id="LastProject">
                 <h2>MES DERNIERS PROJETS<h2>
-                    <article>
+                    <article id="card1">
+                        <h3>APPLICATION SANTE<h3>
+                            <p>Pharmacie de Maurepas</p>
                     </article>
-
-                    <article>
+                    <article id="card2">
+                        <h3>SITE E-COMMERCE</h3>
+                            <p>Librairie l'écume des jours</p>
                     </article>
-
-                    <article>
+                    <article id="card3">
+                        <h3>SITE VITRINE</h3>
+                            <p>Boulangerie Galtier</p>
                     </article>
-
-                    <article>
+                    <article id="card4">
+                        <h3>SITE INSTITUTIONNEL</h3>
+                            <p>Marie de Ploutruc</p>
                     </article>
-
-                    <article>
+                    <article id="card5">
+                        <h3>SITE PROMOTIONNEL</h3>
+                            <p>Festival des choses</p>
                     </article>
             </section>
         </main>
 
-        <footer>
+        <footer id="footer">
             <h2>ME CONTACTER</h2>
 
-            <form>
+            <form id="formulaire">
                 <label>Prénom, Nom*</label>
                 <input></input>
 
@@ -69,6 +120,8 @@
 
                 <label>Message</label>
                 <textarea></textarea>
+
+                <button name="button">Envoyer</button>
             </form>
 
         </footer>
